162caf42e1f8bf820a925b3e6b3db48a8433d8c5
DanielCPimentel <dcp_2030@hotmail.com>
2021-02-11 21:46:14 -0300

    First commit

---
+++ R/aritmetica.R
@@ -0,0 +1,31 @@
+#' Function to sum
+#'
+#' This function is good to sum.
+#'
+#' @param a  A number.
+#' @param b It is another number
+#'
+#' @examples
+#' sum(2, 3)
+#' @export
+#Exporta a função para NAMESPACE
+somar <-  function(a, b) {
+  sum(a, b)
+}
+
+#'Function to subtract
+#'
+#'This function is good to subtract
+#'
+#'@exampes
+#'subtrair(2, 3)
+#'
+#' @export
+subtrair <-  function(a, b){
+  a - b
+}
+
+
+
+
+
